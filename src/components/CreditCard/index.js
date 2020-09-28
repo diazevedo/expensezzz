f5e003ae5a54e3b92245bc8feb4932962987f311
Diego Azevedo <dapptn@hotmail.com>
2020-09-28 23:41:41 +1000

    last numbers, owner, expired date props to CC comp

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -4,21 +4,29 @@ import logo from '../../assets/icons/visa-pay-logo.svg';
 
 import * as S from './styles';
 
-const CreditCard = () => {
+const CreditCard = ({
+  lastNumbers = '4765',
+  owner = 'Stephen Austin',
+  month = '04',
+  year = '21',
+}) => {
   return (
     <S.Container>
       <S.CardLogo src={logo} />
       <S.Details>
         <S.Numbers>
-          <span>＊＊＊＊ ＊＊＊＊ ＊＊＊＊ </span>4765
+          <span>＊＊＊＊ ＊＊＊＊ ＊＊＊＊ </span>
+          {lastNumbers}
         </S.Numbers>
         <S.TextWrapper align="left">
           <S.Text>card holder</S.Text>
-          <S.Text>Stephen Austin</S.Text>
+          <S.Text>{owner}</S.Text>
         </S.TextWrapper>
         <S.TextWrapper>
           <S.Text align="right">valid thru</S.Text>
-          <S.Text align="right">04 / 21</S.Text>
+          <S.Text align="right">
+            {month} / {year}
+          </S.Text>
         </S.TextWrapper>
       </S.Details>
     </S.Container>
